abd7d5682b6ccc9a0fcf530f860683736e45115f
Jayvon Rhines <jayvonrhines0@gmail.com>
2023-04-02 21:01:36 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+   if (400 >= value){
+      return "This one is on me!";
+   } else if (value <= 2000){
+      return "That will be twenty bucks.";
+   } else if (2001 >= value){
+      return "I will gladly take your thirty bucks.";
+   } else if (value >= 2500){
+      return "No can do.";
+   }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(isCity){
   // Write your code here!
+  const city = "NYC";
+  return (isCity === city ? "Ok, sounds good." : "No go.")
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+  }
 }
